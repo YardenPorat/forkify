6e3cd3933adb2dd926ea74cd41e7d08b05717cc9
Yarden Porat <yardenporat@gmail.com>
2021-09-19 09:35:33 +0300

    remove redundant folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,27 @@
-const path = require('path'); // importing path method
+const path = require('path');
 let HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/js/index.js',
     devtool: 'source-map',
     output: {
-        path: path.resolve(__dirname, 'dist'), //where to put bundled file
-        filename: 'js/bundle.js', //bundled file name
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/bundle.js',
     },
     devServer: {
         contentBase: './dist',
     },
     plugins: [
         new HtmlWebpackPlugin({
-            filename: 'index.html', //output filename
-            template: './src/index.html', //starting template
+            filename: 'index.html',
+            template: './src/index.html',
         }),
     ],
     module: {
         rules: [
             {
-                test: /\.js$/, //regex
-                exclude: /node_module/, //exclude node modules
+                test: /\.js$/,
+                exclude: /node_module/,
                 use: {
                     loader: 'babel-loader',
                 },
