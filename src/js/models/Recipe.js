30f182052040242eaafd8b2048c9dde904c1701c
Yarden Porat <yardenporat@gmail.com>
2024-06-29 10:52:59 +0300

    infra: security deps & rebuild

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -1,132 +1,148 @@
-import axios from 'axios';
+import axios from "axios";
 
 export default class Recipe {
-    constructor(id) {
-        this.id=id;
+  constructor(id) {
+    this.id = id;
+  }
+
+  async getRecipe() {
+    const url = "https://forkify-api.herokuapp.com/api/get";
+    try {
+      const res = await axios(`${url}?rId=${this.id}`);
+      this.title = res.data.recipe.title;
+      this.author = res.data.recipe.publisher;
+      this.img = res.data.recipe.image_url;
+      this.ingredients = res.data.recipe.ingredients;
+      this.url = res.data.recipe.source_url;
+    } catch (error) {
+      console.log("something wen't wrong: " + error);
     }
+  }
 
-    async getRecipe() {
-        const url = 'https://forkify-api.herokuapp.com/api/get'
-        try {
-            const res = await axios(`${url}?rId=${this.id}`);
-            this.title = res.data.recipe.title;
-            this.author = res.data.recipe.publisher;
-            this.img = res.data.recipe.image_url;
-            this.ingredients = res.data.recipe.ingredients;
-            this.url = res.data.recipe.source_url;
-            
+  calcTime() {
+    const numIng = this.ingredients.length;
+    const periods = Math.ceil(numIng / 3);
+    this.time = periods * 15;
+  }
 
-        } catch (error){
-            console.log("something wen't wrong: " +error)
-        }
-    }
+  calcServings() {
+    this.servings = 4;
+  }
 
-    calcTime() {
-        const numIng = this.ingredients.length;
-        const periods = Math.ceil(numIng/3);
-        this.time = periods * 15;
-    }
+  parseIngredients() {
+    const unitLong = [
+      "tablespoons",
+      "tablespoon",
+      "whole medium",
+      "whole",
+      "ounces",
+      "ounce",
+      "cups",
+      "pounds",
+      "teaspoons",
+      "teaspoon",
+      "package",
+    ];
+    const unitShort = [
+      "tbs",
+      "tbs",
+      "",
+      "",
+      "oz",
+      "oz",
+      "cup",
+      "pound",
+      "tsp",
+      "tsp",
+      "pack",
+    ];
+    const countLong = ["1/4", " 3/4", " 1/2", "1/2", ","];
+    const countShort = [".25", ".75", ".5", "0.5", ""];
+    const exceptions = ["butter cup", "____"];
+    const filterOut = ["for the", "____"];
+    //Arr of Str
 
-    calcServings() {
-        this.servings = 4; 
-    }
+    let newIngredients = this.ingredients;
+    console.log(newIngredients);
 
-    parseIngredients(){
+    // filter out any ingredient which includes filterOut arr
+    newIngredients = newIngredients.filter(
+      (e) => !filterOut.some((e2) => e.toLowerCase().includes(e2))
+    );
 
-        const unitLong = ['tablespoons' ,'tablespoon', 'whole medium','whole','ounces' ,'ounce' ,'cups','pounds','teaspoons' ,'teaspoon' ,'package',]
-        const unitShort = ['tbs'        ,'tbs'       , ''        ,'' ,'oz'     ,'oz'    ,'cup' ,'pound' ,'tsp','tsp'      ,'pack'  ]
-        const countLong =  [ '1/4' ,' 3/4', ' 1/2','1/2',',']
-        const countShort = [ '.25' ,'.75' , '.5', '0.5' ,'']
-        const exceptions = ['butter cup','____']
-        const filterOut = ['for the', '____']
-        //Arr of Str
-        
+    newIngredients = newIngredients.map((el) => {
+      //all ing to lowercase
+      let ingredient = el.toLowerCase();
 
-        let newIngredients = this.ingredients 
-        console.log(newIngredients);
-        
-        
-        // filter out any ingredient which includes filterOut arr
-        newIngredients = newIngredients.filter( e => !filterOut.some(e2 => e.toLowerCase().includes(e2)));
-        
-        newIngredients = newIngredients.map( el =>{
+      //uniform units
+      unitLong.forEach((unit, i) => {
+        ingredient = ingredient.replace(unit, unitShort[i]);
+      });
+      //uniform count (remove fractions)
+      countLong.forEach((unit, i) => {
+        ingredient = ingredient.replace(unit, countShort[i]);
+      });
 
-            //all ing to lowercase
-            let ingredient =el.toLowerCase();
+      // remove parenthesis
+      ingredient = ingredient.replace(/ *\([^)]*\) */g, " ").trim();
 
-            //uniform units
-            unitLong.forEach( (unit,i) => {
-                ingredient=ingredient.replace(unit,unitShort[i]);
-            });
-            //uniform count (remove fractions)
-            countLong.forEach( (unit,i) => {
-                ingredient=ingredient.replace(unit,countShort[i]);
-            });
-            
-            // remove parethesis
-            ingredient=ingredient.replace(/ *\([^)]*\) */g,' ');
+      //// Parse ingredients into count, unit, & ingredients
 
-            //// any whitespaces start, then (, all until != )
-            
-            ingredient=ingredient.trim();
-            
-            //// Parse ingredients into count, unit, & ingredients
-            
-            //find unit index
-            const arrIng = ingredient.split(' ');
-            let unitIndex;
-            const exception = exceptions.some(e => ingredient.includes(e));
-            //console.log(exception)
+      //find unit index
+      const arrIng = ingredient.split(" ");
+      let unitIndex;
+      const exception = exceptions.some((e) => ingredient.includes(e));
+      //console.log(exception)
 
-            if (!exception) 
-            {
-                unitIndex = arrIng.findIndex( (e2) => {
-                    return (unitShort.includes(e2));
-                });
-            } else {
-                unitIndex = -1
-            }
-            
-            let objIng;
-            if (unitIndex>-1) {
-                //unit was found
-                objIng = {
-                    count: parseFloat(arrIng.slice(0,unitIndex).join(' ')) || 1,
-                    unit: arrIng[unitIndex],
-                    ingredient: arrIng.slice(2).join(' ')
-                }
-            } else if(!isNaN(arrIng[0])) {
-                objIng = {
-                    count: parseFloat(arrIng[0]),
-                    unit: '',
-                    ingredient: arrIng.slice(1).join(' ')
-                }
-             
-            } else if (unitIndex===-1) {
-                //no unit
-                objIng = {
-                    count: 1,
-                    unit: '',
-                    ingredient //ingredient: ingredient
-                }
-            }
-
-            return objIng;
-          
+      if (!exception) {
+        unitIndex = arrIng.findIndex((e2) => {
+          return unitShort.includes(e2);
         });
-        this.ingredients=newIngredients;
-    }
+      } else {
+        unitIndex = -1;
+      }
 
-    updateServings(type) {
-        // Servings 
-        const newServings = (type ==='dec' ? this.servings-1 : this.servings+1);
-        
-        //Ingredients & time
-        this.ingredients.forEach(ing => {
-            ing.count = Math.round(((ing.count*(newServings / this.servings)) + Number.EPSILON) * 100) / 100;
-            //(ing.count*(newServings / this.servings));
-        });
-        this.time = Math.round(this.time*(newServings / this.servings));
-        this.servings=newServings;
-    }
+      let objIng;
+      if (unitIndex > -1) {
+        //unit was found
+        objIng = {
+          count: parseFloat(arrIng.slice(0, unitIndex).join(" ")) || 1,
+          unit: arrIng[unitIndex],
+          ingredient: arrIng.slice(2).join(" "),
+        };
+      } else if (!isNaN(arrIng[0])) {
+        objIng = {
+          count: parseFloat(arrIng[0]),
+          unit: "",
+          ingredient: arrIng.slice(1).join(" "),
+        };
+      } else if (unitIndex === -1) {
+        //no unit
+        objIng = {
+          count: 1,
+          unit: "",
+          ingredient, //ingredient: ingredient
+        };
+      }
+
+      return objIng;
+    });
+    this.ingredients = newIngredients;
+  }
+
+  updateServings(type) {
+    // Servings
+    const newServings = type === "dec" ? this.servings - 1 : this.servings + 1;
+
+    //Ingredients & time
+    this.ingredients.forEach((ing) => {
+      ing.count =
+        Math.round(
+          (ing.count * (newServings / this.servings) + Number.EPSILON) * 100
+        ) / 100;
+      //(ing.count*(newServings / this.servings));
+    });
+    this.time = Math.round(this.time * (newServings / this.servings));
+    this.servings = newServings;
+  }
 }
